b5066d9431d674c5521116528dfb591d8a05d06d
lucas95123 <1245554637@qq.com>
2016-05-22 20:46:50 +0800

    Implemented chatting function basics

--- routes/chatServer.js
+++ routes/chatServer.js
@@ -1,11 +1,9 @@
 var io = require('socket.io')();
 
-io.on('connection', function (_socket) {
-    console.log(_socket.id + ': connection');
-    _socket.on('message', function (msg) {
-        console.log('Message Received: ', msg);
-        _socket.broadcast.emit('message', msg);
-    });
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 exports.listen = function (_server) {
