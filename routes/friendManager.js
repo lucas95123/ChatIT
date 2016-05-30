a22e2f10e1ff00b4355d727336c4de5c4cc08cef
lucas95123 <lucas95123@outlook.com>
2016-05-30 20:06:58 +0800

    Group friends

--- routes/friendManager.js
+++ routes/friendManager.js
@@ -1,7 +1,7 @@
 var searchManager = require('./searchManager');
 
 exports.getFriends = function(user_id, callback) {
-    sql = 'select A.user_id,user_name,tag,email_addr from AccountInfo as A join FriendInfo as B where B.user_id='
+    sql = 'select B.user_id,A.user_id as friend_id,user_name,tag,email_addr from AccountInfo as A join FriendInfo as B where B.user_id='
     sql += user_id;
     sql += ' and A.user_id=B.friend_id order by tag;';
 
@@ -48,6 +48,22 @@ exports.deleteFriends = function(user_id, friend_id, callback) {
     sql = 'delete from FriendInfo where user_id=';
     sql += user_id + ' and friend_id=';
     sql += friend_id + ';';
+    console.log(sql);
+    searchManager.query(sql, function(qerr, vals) {
+        if (qerr) {
+            callback(qerr);
+            return;
+        } else {
+            callback(qerr, vals);
+        }
+    })
+}
+
+exports.changeTag = function(user_id, friend_id, tag, callback) {
+    sql = 'update FriendInfo set tag=';
+    sql+='\''+tag+'\' where user_id=';
+    sql+=user_id+" and friend_id=";
+    sql+=friend_id;
     console.log(sql);
     searchManager.query(sql, function(qerr, vals) {
         if (qerr) {
